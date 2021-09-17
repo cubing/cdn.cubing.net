a8d5e33e2b07e7e77cb2f4c36da4c2f1ed48bb6d
Lucas Garron <code@garron.net>
2021-09-16 23:42:51 -0700

    Add an import map script to simulate dynamic imports.
    This is a super bad idea for any real apps, but fun to play with.
    See https://twitter.com/lgarron/status/1438742427133612032
    
    Usage:
    
    ```
    <script src="https://cdn.cubing.net/js/import-map-hack.js"></script>
    
    <script type="module" defer>
      import { TwistyPlayer } from "cubing/twisty";
      document.body.appendChild(new TwistyPlayer({alg: "[R, U]"}));
    </script>
    ```

---
+++ src/static/js/import-map-hack.js
@@ -0,0 +1,30 @@
+// See https://twitter.com/lgarron/status/1438742427133612032
+
+const importMap = {
+  imports: {
+    "cubing/alg": "https://cdn.cubing.net/js/cubing/alg",
+    "cubing/bluetooth": "https://cdn.cubing.net/js/cubing/bluetooth",
+    "cubing/kpuzzle": "https://cdn.cubing.net/js/cubing/kpuzzle",
+    "cubing/notation": "https://cdn.cubing.net/js/cubing/notation",
+    "cubing/protocol": "https://cdn.cubing.net/js/cubing/protocol",
+    "cubing/puzzle-geometry":
+      "https://cdn.cubing.net/js/cubing/puzzle-geometry",
+    "cubing/puzzles": "https://cdn.cubing.net/js/cubing/puzzles",
+    "cubing/scramble": "https://cdn.cubing.net/js/cubing/scramble",
+    "cubing/search": "https://cdn.cubing.net/js/cubing/search",
+    "cubing/stream": "https://cdn.cubing.net/js/cubing/stream",
+    "cubing/twisty": "https://cdn.cubing.net/js/cubing/twisty",
+    "scramble-display": "https://cdn.cubing.net/js/scramble-display",
+    scrambles: "https://cdn.cubing.net/js/scrambles",
+    three: "https://cdn.cubing.net/js/three",
+  },
+};
+const script = document.createElement("script");
+script.type = "importmap";
+script.textContent = JSON.stringify(importMap);
+
+console.warn(
+  "Loading the hacky cdn.cubing.net import map. This is experimental, only works in Chrome, and WILL slow down your page. Do not use it for any real apps."
+);
+
+document.write(new XMLSerializer().serializeToString(script));
