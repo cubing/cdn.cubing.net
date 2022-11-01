3352437f214d45b804af00a61a1c425753e989e6
Lucas Garron <code@garron.net>
2022-10-31 20:45:10 -0700

    [speedsolving] Remove `console.log` statements.

--- src/static/js/@cubing/porcelain/speedsolving-forum/twizzle-link.js
+++ src/static/js/@cubing/porcelain/speedsolving-forum/twizzle-link.js
@@ -4,17 +4,13 @@ if (!globalThis.twizzleLinkScript) {
   script.src = "https://cdn.cubing.net/js/cubing/twisty";
   script.type = "module";
 
-  console.log("script is", script);
   function append() {
-    console.log("appending");
     document.body.appendChild(script);
   }
 
   if (document.body) {
-    console.log("document.body exists");
     append();
   } else {
-    console.log("listener");
     window.addEventListener("DOMContentLoaded", append);
   }
 }
