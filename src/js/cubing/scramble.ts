38c8eac693c1e898cea997c623a0b31043c472fc
Lucas Garron <code@garron.net>
2023-07-23 21:33:43 -0700

    Set `{ showWorkerInstantiationWarnings: false }`

--- src/js/cubing/scramble.ts
+++ src/js/cubing/scramble.ts
@@ -1,1 +1,4 @@
+import { setSearchDebug } from "cubing/search";
+setSearchDebug({ showWorkerInstantiationWarnings: false }); // We take responsibility for this on the CDN.
+
 export * from "cubing/scramble";
